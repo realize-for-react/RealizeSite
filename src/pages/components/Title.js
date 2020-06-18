7af4e63f160788486b1bd65e370fc50ded98baed
Fan <fanny.shao18@gmail.com>
2020-06-18 11:10:10 -0400

    diff img again

--- src/pages/components/Title.js
+++ src/pages/components/Title.js
@@ -6,7 +6,7 @@ export default function Title() {
   return (
     <div className="title"><a id="title"></a>
       <div className="inner-align">
-        <img className="logoGif" width="25%" src="https://i.imgur.com/oJcCqL9.gif" />
+        <img className="logoGif" width="25%" src={gifLogo} />
         <div className="main-title">Realize</div>
         <div className="sub-title">Browser extension to visualize React state flow and component hierarchy</div>
 
